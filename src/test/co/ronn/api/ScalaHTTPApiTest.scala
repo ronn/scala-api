7e055a98ce0b05a14c329233268d02fbefa6317c
ronn <ronniecanas@seven4n.com>
2018-08-13 19:07:09 -0500

    First test for routes with Akka Http

--- src/test/co/ronn/api/ScalaHTTPApiTest.scala
+++ src/test/co/ronn/api/ScalaHTTPApiTest.scala
@@ -1,5 +1,45 @@
 package co.ronn.api
 
-class ScalaHTTPApiTest {
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{Matchers, WordSpec}
 
+
+final class ScalaHTTPApiTest extends WordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
+
+  private val routesWithDefin1edResponses: Route =
+    get {
+      path("status"){
+        complete(HttpEntity(ContentTypes.`application/json`, """{"status": "ok"}"""))
+      }
+    }
+
+  private val greetingRoute: Route = {
+    get{
+      path("saludo"){
+        complete("Hola, putos!")
+      }
+    }
+  }
+
+  "ScalaHTTPApiTest" should{
+
+    "respond successfuly while requesting its  status" in {
+
+      Get("/status") ~> routesWithDefin1edResponses ~> check{
+        status shouldBe StatusCodes.OK
+        contentType shouldBe ContentTypes.`application/json`
+        entityAs[String] shouldBe """{"status": "ok"}"""
+      }
+
+      Get("/saludo") ~> greetingRoute ~> check{
+        status shouldBe StatusCodes.OK
+        entityAs[String] shouldBe "Hola, putos!"
+        contentType shouldBe ContentTypes.`text/plain(UTF-8)`
+      }
+    }
+  }
 }
