61b5179ee44d86ea6ab4ae5394bfc7fb2b925d48
ronn <ronniecanas@seven4n.com>
2018-08-25 17:56:27 -0500

    Agrego ruta del server

--- src/main/co/ronn/api/Routes.scala
+++ src/main/co/ronn/api/Routes.scala
@@ -1,5 +1,14 @@
 package co.ronn.api
 
-class Routes {
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives.{complete, get, path}
+import akka.http.scaladsl.server.Route
 
+object Routes {
+
+  val all: Route = get {
+    path("status"){
+      complete(HttpEntity(ContentTypes.`application/json`, """{"status": "ok"}"""))
+    }
+  }
 }
